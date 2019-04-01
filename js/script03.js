1f9d14681567c2d8d78de86122f897fddaf0585c
magneat <1100011@ukr.net>
2019-04-02 02:45:13 +0300

    many changes. including successfull horisontal blocks, bgColor change and an idea of table positioning.

--- js/script03.js
+++ js/script03.js
@@ -1,5 +1,9 @@
-/*
-function BgWhite1() {
-
-}
-*/
+var person = {
+	firstName: "Mike",
+	lastName: "Buggernaut",
+	id: 5566,
+	fullName: function() {
+		return this.firstName + " " + this.lastName;
+	}
+};
+document.getElementById("par3").innerHTML = person.fullName();
