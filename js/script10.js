125e126e6dc0df81150c6a363b3e4b4d960dbc0c
magneat <1100011@ukr.net>
2019-04-03 04:37:24 +0300

    added feat.11

--- js/script10.js
+++ js/script10.js
@@ -1,10 +1,8 @@
 //вводим в буфер фразу для счёта
-var str = document.getElementById("h2_10").innerHTML;
+var str10 = document.getElementById("h2_10").innerHTML;
 
 //вводим слово, до которого нужно сосчитать символы
-var pos = str.lastIndexOf("website");
+var pos = str10.search("count");
 
 //выводим куда выдать ответ
 document.getElementById("spn10").innerHTML = pos;
-
-index
---
+++ js/script11.js
@@ -0,0 +1,5 @@
+var str11 = document.getElementById("h2_11").innerHTML;
+var count11 = str11.length;
+document.getElementById("spn11").innerHTML = count11;
+var slice11 = str11.slice(35, 54);
+document.getElementById("spn112").innerHTML = slice11;
