41548ce468c60ad31cd414608872287ce83a78bb
magneat <1100011@ukr.net>
2019-04-03 04:59:38 +0300

    fixed two features no 10 and trying out git commit -a -m

--- js/script10.js
+++
@@ -1,8 +0,0 @@
-//вводим в буфер фразу для счёта
-var str10 = document.getElementById("h2_10").innerHTML;
-
-//вводим слово, до которого нужно сосчитать символы
-var pos = str10.search("count");
-
-//выводим куда выдать ответ
-document.getElementById("spn10").innerHTML = pos;
--- js/script11.js
+++ js/script11.js
@@ -1,5 +1,8 @@
+//вводим в буфер фразу для счёта
 var str11 = document.getElementById("h2_11").innerHTML;
-var count11 = str11.length;
-document.getElementById("spn11").innerHTML = count11;
-var slice11 = str11.slice(35, 54);
-document.getElementById("spn112").innerHTML = slice11;
+
+//вводим слово, до которого нужно сосчитать символы
+var pos = str11.search("count");
+
+//выводим куда выдать ответ
+document.getElementById("spn11").innerHTML = pos;
