bb40a4ed6ef537600be17a05d263938424e1a9b3
magneat <1100011@ukr.net>
2019-04-05 15:06:55 +0300

    1st attempt to clean master branch

--- js/z_failscript02.js
+++
@@ -1,9 +0,0 @@
-var person = {
-  firstName : "Dude",
-  lastName  : "Random",
-  nickname  : "Uhe"
-  address   : "15th Str, Al"
-  id        : 223322,
-  fullName  : function() {
-    return this.firstName + " " + this.lastName;
-  }
